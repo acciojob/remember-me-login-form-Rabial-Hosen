b1a9ac367087f1f21a33513e13c87f8c67b8602c
Rabial_Hosen <hosenrabial@gmail.com>
2023-06-22 14:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,93 @@
 //your JS code here. If required.
-const form = document.querySelector("form");
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
-const rememberMeCheckbox = document.querySelector("#checkbox");
-const exisitingButton = document.querySelector("#existing");
+const questions = [
+  {
+    question: "What is the capital of France?",
+    choices: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "What is the highest mountain in the world?",
+    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
+    answer: "Everest",
+  },
+  {
+    question: "What is the largest country by area?",
+    choices: ["Russia", "China", "Canada", "United States"],
+    answer: "Russia",
+  },
+  {
+    question: "Which is the largest planet in our solar system?",
+    choices: ["Earth", "Jupiter", "Mars"],
+    answer: "Jupiter",
+  },
+  {
+    question: "What is the capital of Canada?",
+    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+    answer: "Ottawa",
+  },
+];
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-  const rememberMe = rememberMeCheckbox.checked;
+const submitButton = document.getElementById("submit");
+const questionsElement = document.getElementById("questions");
 
-  if (rememberMe) {
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-  } else {
-    localStorage.removeItem("username");
-    localStorage.removeItem("password");
-  }
+// Get the saved progress from session storage
+const savedProgress = JSON.parse(sessionStorage.getItem("progress"));
+// Submit the quiz and show the score
+submitButton.addEventListener("click", showScore);
 
-  alert(`Logged in as ${username}`);
-  showButton();
+// If there is saved progress, use it. Otherwise, start from the beginning.
+let userAnswers = savedProgress ? savedProgress : [];
 
-  form.reset(); // Clear the form fields
-});
-
-function loginAsSaved() {
-  const username = localStorage.getItem("username");
-  const password = localStorage.getItem("password");
-
-  if (username && password) {
-    alert(`Logged in as ${username}`);
-    form.reset();
+// Display the quiz questions and choices
+function renderQuestions() {
+  for (let i = 0; i < questions.length; i++) {
+    const question = questions[i];
+    const questionElement = document.createElement("div");
+    const questionText = document.createTextNode(question.question);
+    questionElement.appendChild(questionText);
+    for (let j = 0; j < question.choices.length; j++) {
+      const choice = question.choices[j];
+      const choiceElement = document.createElement("input");
+      choiceElement.setAttribute("type", "radio");
+      choiceElement.setAttribute("name", `question-${i}`);
+      choiceElement.setAttribute("value", choice);
+      if (userAnswers[i] === choice) {
+        choiceElement.setAttribute("checked", true);
+      }
+      const choiceText = document.createTextNode(choice);
+      questionElement.appendChild(choiceElement);
+      questionElement.appendChild(choiceText);
+    }
+    questionsElement.appendChild(questionElement);
   }
 }
 
-function showButton() {
-  const username = localStorage.getItem("username");
-  const password = localStorage.getItem("password");
-
-  if (username && password) {
-    exisitingButton.style.display = "block";
-  } else {
-    exisitingButton.style.display = "none";
+// Save the user's answer when they select a choice
+function saveAnswers() {
+  const choiceElements = document.querySelectorAll("input[type=radio]");
+  for (let i = 0; i < choiceElements.length; i++) {
+    const choiceElement = choiceElements[i];
+    choiceElement.addEventListener("change", function (event) {
+      const answer = event.target.value;
+      userAnswers[parseInt(event.target.name.split("-")[1], 10)] = answer;
+      sessionStorage.setItem("progress", JSON.stringify(userAnswers));
+    });
   }
 }
 
-exisitingButton.style.display = "none";
-exisitingButton.addEventListener("click", loginAsSaved);
-showButton();
+function showScore() {
+  let score = 0;
+  for (let i = 0; i < questions.length; i++) {
+    const question = questions[i];
+    const userAnswer = userAnswers[i];
+    if (userAnswer === question.answer) {
+      score++;
+    }
+  }
+  localStorage.setItem("score", score);
+  const scoreElement = document.getElementById("score");
+  scoreElement.innerText = `Your score is ${score} out of ${questions.length}.`;
+}
+
+renderQuestions();
+saveAnswers();
